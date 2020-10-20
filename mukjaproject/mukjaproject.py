8aa5b9e137c47d1ac9f8e83a03d1dc486c8cf2ae
ltyiz07 <ltyiz07@gmail.com>
2020-10-20 12:22:40 +0900

    separate python files for function -TY

--- mukjaproject/mukjaproject.py
+++ mukjaproject/mukjaproject.py
@@ -1,49 +1,13 @@
 from flask import Flask, render_template, request
 import mariadb
 import sys
+
+from python.add_tag import add_tag_link     # add_tag_link(store_id, tag_name)
+from python.get_ import get_conn, get_tag, get_img
+# get_tag(store_id), get_img(store_id)
+
+
 app = Flask(__name__)
-
-# connect to mariadb platform
-def get_conn():
-    try:
-        conn = mariadb.connect(
-            user="ubuntu",
-            password="0625",
-            host="193.123.250.119",
-            port=3306,
-            database="mukja_db2"
-        )
-    except mariadb.Error as e:
-        print(f"error connecting to mariadb platform: {e}")
-        sys.exit(1)
-    return conn
-
-def get_tag(store_id=0):
-    """
-    input:  스토어 아이디
-    return: 아이디에 해당되는 태그들 모두
-    """
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT tag_name FROM tag WHERE tag_id in (SELECT tag_id FROM tag_link WHERE store_id = ?);", (store_id,))
-    tags = []
-    for t in cur:
-        tags.append(t)
-    return tags
-
-def get_img(store_id=0):
-    """
-    input: 스토어 아이디
-    return:  스토어 아이디에 해당되는 사진들 모두 (사진이름, 순서)
-    """
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT img_name, img_order FROM image WHERE store_id=? ORDER BY img_order ASC", (store_id,))
-    imgs = []
-    for (img_name, img_order) in cur:
-        imgs.append((img_name, img_order))
-    return imgs
-
 
 @app.route("/")
 def main():
---
+++ mukjaproject/python/get_.py
@@ -0,0 +1,48 @@
+"""
+assign function for using mukja_db2
+"""
+import mariadb
+import sys
+
+
+def get_conn():
+    try:
+        conn = mariadb.connect(
+            user="ubuntu",
+            password="0625",
+            host="193.123.250.119",
+            port=3306,
+            database="mukja_db2"
+        )
+    except mariadb.Error as e:
+        print(f"error connecting to mariadb platform: {e}")
+        sys.exit(1)
+    return conn
+
+
+def get_tag(store_id=0):
+    """
+    input:  스토어 아이디
+    return: 아이디에 해당되는 태그들 모두
+    """
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT tag_name FROM tag WHERE tag_id in (SELECT tag_id FROM tag_link WHERE store_id = ?);", (store_id,))
+    tags = []
+    for t in cur:
+        tags.append(t)
+    return tags
+
+
+def get_img(store_id=0):
+    """
+    input: 스토어 아이디
+    return:  스토어 아이디에 해당되는 사진들 모두 (사진이름, 순서)
+    """
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT img_name, img_order FROM image WHERE store_id=? ORDER BY img_order ASC", (store_id,))
+    imgs = []
+    for (img_name, img_order) in cur:
+        imgs.append((img_name, img_order))
+    return imgs
