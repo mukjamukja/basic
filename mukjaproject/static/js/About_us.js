c9e2bd22106de08d4f21c28dab78166483c8cc7a
mhj401 <mhj401@gmail.com>
2020-10-26 12:10:14 +0900

    201026(이제top버튼나옴)_HJ

--- mukjaproject/static/js/About_us.js
+++ mukjaproject/static/js/About_us.js
@@ -2,29 +2,13 @@ const mainbtn = document.querySelector('.mainbar__menubtn');
 const mainmenu = document.querySelector('.mainbar__menu');
 const mainlinks = document.querySelector('.mainbar__links');
 
-const sortbtn = document.querySelector('.sectionbar__sortbtn');
-const sortbtnmenu = document.querySelector('.sortbtn__menu');
-
-const gridbtn = document.querySelector('.sectionbar__gridbtn');
-const mapbtn = document.querySelector('.sectionbar__mapbtn');
-
 
 mainbtn.addEventListener('click', ()=> {
     mainmenu.classList.toggle('active');
     mainlinks.classList.toggle('active');
 });
 
-gridbtn.addEventListener('click', () => {
-    gridbtn.classList.toggle('active')
-})
 
-mapbtn.addEventListener('click', ()=> {
-    mapbtn.classList.toggle('active')
-})
-
-sortbtn.addEventListener('click', ()=> {
-    sortbtnmenu.classList.toggle('active');
-});
 
 $( document ).ready( function() {
     $( window ).scroll( function() {
@@ -40,86 +24,6 @@ $( document ).ready( function() {
     } );
   } );
 
-// from here for sorting (isotope)
-// init Isotope
-// var $grid_view = $('.grid_view').isotope({
-//   itemSelector: '.grid_view-item',
-//   getSortData: {
-//     score: '.score',
-//     distance: '.distance',
-//     score: '.score parseInt',
-//     tag: '[data-tag]',
-//   };
-// });
-
-
-// external js: isotope.pkgd.js
-
-// // init Isotope
-// var $grid = $('.grid').isotope({
-//   itemSelector: '.color-shape'
-// });
-
-// // store filter for each group
-// var filters = {};
-
-// $('.filters').on( 'click', '.button', function( event ) {
-//   var $button = $( event.currentTarget );
-//   // get group key
-//   var $buttonGroup = $button.parents('.button-group');
-//   var filterGroup = $buttonGroup.attr('data-filter-group');
-//   // set filter for group
-//   filters[ filterGroup ] = $button.attr('data-filter');
-//   // combine filters
-//   var filterValue = concatValues( filters );
-//   // set filter for Isotope
-//   $grid.isotope({ filter: filterValue });
-// });
-
-// // change is-checked class on buttons
-// $('.button-group').each( function( i, buttonGroup ) {
-//   var $buttonGroup = $( buttonGroup );
-//   $buttonGroup.on( 'click', 'button', function( event ) {
-//     $buttonGroup.find('.is-checked').removeClass('is-checked');
-//     var $button = $( event.currentTarget );
-//     $button.addClass('is-checked');
-//   });
-// });
-  
-// // flatten object by concatting values
-// function concatValues( obj ) {
-//   var value = '';
-//   for ( var prop in obj ) {
-//     value += obj[ prop ];
-//   }
-//   return value;
-// }
-
-// init isotope
-
-var $grid_view = $(".grid_view").isotope({
-  itemSelector: '.grid_view-item'
-});
-var filters = {};
-
-$('.filters').on('click', '.button', function( event ){
-  var $button = $( event.currentTarget );
-
-  var $tagFilter = $button.parents('.tag_filter');
-  var filterGroup = $tagFilter.attr('data-filter-group');
-  filters[ filterGroup ] = $button.attr('data-filter');
-  var filterValue = concatValues( filters );
-  $grid_view.isotope({ filter: filterValue });
-});
-
-$('.tag_filter').each( function( i, filterGroup ) {
-  var $filterGroup = $( filterGroup );
-  $filterGroup.on( 'click', 'button', function ( event ) {
-    $filterGroup.find('.is-checked').removeClass('is-checked');
-    var $button = $( event.currentTarget );
-    $button.addClass('is-checked');
-  });
-});
 
 function concatValues( obj ) {
   var value = '';
